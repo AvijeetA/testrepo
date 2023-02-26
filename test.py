a44994f2f9223e078522bd9f0f27f62e5a90acf5
AvijeetA <97691356+AvijeetA@users.noreply.github.com>
2023-02-26 23:24:01 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+Date	Version	Changed by	Change Description
+2023-01-17	0.6	Steve Hord	QA pass with edits
+2020-07-16	0.5	Malika Singla	Spell check, step number added
+2020-07-14	0.4	Rav Ahuja	Changed logo, updated effort, title, intro, objectives, added Authors and Changelog
+2020-07-13	0.3	Malika Singla	Added to GitLab and made some formatting changes, added objectives, etc.
+2020-07-03	0.2	Malika Singla	Ported to markdown and added GitHub account signup, new screenshots, etc.
+2020-06-30	0.1	Romeo Kienzler	Drafted initial version
